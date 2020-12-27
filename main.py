b09b19e719b7b07fcbfa25c6feb2f61819def1e7
AWhiteFox <ignat.k.150@gmail.com>
2020-12-27 19:45:07 +0300

    Add main.py draft

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from tanks.constants import *
+import tanks.grid as grid
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+all_sprites = pygame.sprite.Group()
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    screen.fill((116, 116, 116))  # gray
+    pygame.draw.rect(screen, 'black', grid.get_rect())
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    delta_time = clock.tick() / 1000
+    all_sprites.update(delta_time)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+pygame.quit()
