bb7273157cf1a5302378e69048fefe04c335cb06
nikit0ska <nik.ryabov.2014@mail.ru>
2021-01-11 20:16:13 +0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import tanks.scenes as scenes
 from tanks.constants import *
 from tanks.time import tick
-from tanks.sprites import Shell
+from tanks.sprites import Shell, Tank
 from random import randint
 
 pygame.init()
@@ -16,7 +16,9 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            Shell(pos[0], pos[1], randint(1, 4), scenes.current_scene().all_sprites)
+            # Shell(pos[0], pos[1], randint(1, 4), scenes.current_scene().all_sprites)
+            Tank(pos[0], pos[1], True, scenes.current_scene().all_sprites)
+            # Tank(pos[0], pos[1]+100, False, scenes.current_scene().all_sprites)
     scenes.current_scene().update()
     scenes.current_scene().draw(screen)
     pygame.display.flip()
