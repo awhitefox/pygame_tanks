7dd5efe6979af381114e5cde5fb5d69c1446c874
AWhiteFox <ignat.k.150@gmail.com>
2021-01-12 16:30:32 +0300

    Finish input.py

--- tanks/input.py
+++ tanks/input.py
@@ -1,17 +1,2 @@
-import pygame
-
-
-def is_pressed(key):
-    return pygame.key.get_pressed()[key]
-
-
-def is_just_pressed(key):
-    return
-
-
-def is_mouse_pressed(key):
-    return pygame.mouse.get_pressed(5)[key]
-
-
-def is_mouse_just_pressed(key):
-    return
+keys_just_pressed = set()
+mouse_keys_just_pressed = set()
