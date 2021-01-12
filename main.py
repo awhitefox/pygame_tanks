d519dcd96d7e4d525eba69bb603610eaa16dfcfe
AWhiteFox <ignat.k.150@gmail.com>
2021-01-12 15:55:08 +0300

    Create LevelSelectMenu

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from tanks.time import tick
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
-scenes.load_scene(scenes.Menu())
+scenes.load_scene(scenes.MainMenu())
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill('black')
     scenes.current_scene().update()
     scenes.current_scene().draw(screen)
     pygame.display.flip()
