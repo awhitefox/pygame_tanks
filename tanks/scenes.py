8f13f5925fe8164b42b8eb6cdb2ff6c1e83f7786
AWhiteFox <ignat.k.150@gmail.com>
2021-01-14 23:29:32 +0300

    Add support for winning and losing

--- tanks/scenes.py
+++ tanks/scenes.py
@@ -5,7 +5,7 @@ import pygame
 import tanks.grid as grid
 from tanks.constants import SCREEN_SIZE, MAP_SIZE
 from tanks.sprites import ConcreteWall, BrickWall, Bush, Water, Spike, Tank
-from tanks.ui import TextButton, Label, GameLogo, font_medium, font_small
+from tanks.ui import TextButton, Label, GameLogo, ScreenMessage, font_medium, font_small
 from tanks.input import mouse_keys_just_pressed
 from random import randint
 
@@ -126,15 +126,57 @@ class LevelSelectMenu(Scene):
 
 
 class Level(Scene):
-    def __init__(self):
+    score_to_win = 3
+
+    def __init__(self, filename, score=None):
         super().__init__()
+        self.filename = filename
+        self.score = score if score else [0, 0]
+        self.game_finished = False
+
         grid_x = MAP_SIZE[0] // 2 - 1
         self.tank1 = Tank(*grid.cell_to_screen(grid_x, MAP_SIZE[1] - 2), True, self.all_sprites)
         self.tank2 = Tank(*grid.cell_to_screen(grid_x, 0), False, self.all_sprites)
-        self.flag = True
+
+        self.start_message = ScreenMessage("Приготовится!", font_medium, 2, self.all_sprites)
+        self.end_message = None
 
     def update(self):
+        if self.start_message.alive():
+            self.start_message.update()
+            return
+        if self.end_message:
+            self.end_message.update()
+            if not self.end_message.alive():
+                if not self.game_finished:
+                    load_scene(Level.load(self.filename, self.score))
+                else:
+                    load_scene(LevelSelectMenu())
+            return
         super().update()
+
+        finish_round = False
+        if not self.tank1.alive():
+            self.score[1] += 1
+            finish_round = True
+        if not self.tank2.alive():
+            self.score[0] += 1
+            finish_round = True
+
+        if finish_round:
+            if self.score == [self.score_to_win, self.score_to_win]:
+                end_message_text = 'Ничья!'
+                self.game_finished = True
+            elif self.score[0] == self.score_to_win:
+                end_message_text = 'Игрок 1 победил!'
+                self.game_finished = True
+            elif self.score[1] == self.score_to_win:
+                end_message_text = 'Игрок 2 победил!'
+                self.game_finished = True
+            else:
+                end_message_text = f'{self.score[0]} : {self.score[1]}'
+            self.end_message = ScreenMessage(end_message_text, font_medium, 3, self.all_sprites)
+            return
 
     def draw(self, surface):
         surface.fill((116, 116, 116))  # gray
@@ -142,8 +184,8 @@ class Level(Scene):
         super().draw(surface)
 
     @classmethod
-    def load(cls, filename):
-        level = cls()
+    def load(cls, filename, score=None):
+        level = cls(filename, score)
         level_map = [list(line.rstrip('\n')) for line in open(os.path.join('levels', filename))]
         for row in range(len(level_map)):
             for col in range(len(level_map[row])):
