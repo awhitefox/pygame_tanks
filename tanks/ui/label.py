54bbc356e08a200dfd39840c0c530c8bebd3e56e
AWhiteFox <ignat.k.150@gmail.com>
2021-01-16 23:33:27 +0300

    Document label.py

--- tanks/ui/label.py
+++ tanks/ui/label.py
@@ -2,7 +2,10 @@ import pygame
 
 
 class Label(pygame.sprite.Sprite):
-    def __init__(self, center_x, center_y, text, font, *groups):
+    """Элемент UI, отображает строку текста на экране."""
+
+    def __init__(self, center_x: float, center_y: float, text: str, font: pygame.font.Font,
+                 *groups: pygame.sprite.Group):
         super().__init__(*groups)
         self.text = text
         self.font = font
@@ -10,7 +13,7 @@ class Label(pygame.sprite.Sprite):
         self.rect.center = center_x, center_y
         self.image = pygame.surface.Surface(self.rect.size)
 
-    def update(self):
+    def update(self) -> None:
         self.image = self.font.render(self.text, True, (255, 255, 255))
         w, h = self.image.get_size()
         self.rect.inflate_ip(w - self.rect.w, h - self.rect.h)
