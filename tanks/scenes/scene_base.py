08eacc835b19f6d18e229ce4de365b635e147005
AWhiteFox <ignat.k.150@gmail.com>
2021-01-16 18:39:12 +0300

    Document scene_base.py

--- tanks/scenes/scene_base.py
+++ tanks/scenes/scene_base.py
@@ -2,14 +2,18 @@ import pygame
 
 
 class SceneBase:
+    """Базовый класс сцены, используется для наследования."""
     def __init__(self):
         self.all_sprites = pygame.sprite.LayeredUpdates()
 
-    def update(self):
+    def update(self) -> None:
+        """Выполняет обновление всех спрайтов на сцене."""
         self.all_sprites.update()
 
-    def draw(self, surface):
+    def draw(self, surface: pygame.Surface) -> None:
+        """Выполняет отрисовку всех спрайтов на сцене."""
         self.all_sprites.draw(surface)
 
-    def teardown(self):
+    def teardown(self) -> None:
+        """Выполняет очистку ресурсов сцены."""
         self.all_sprites.empty()
