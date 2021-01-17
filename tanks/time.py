5a89f439b8ab1fad56ec20790fe653d3be280bd3
AWhiteFox <ignat.k.150@gmail.com>
2021-01-17 12:11:24 +0300

    Document time.py

--- tanks/time.py
+++ tanks/time.py
@@ -3,10 +3,12 @@ _clock = pygame.time.Clock()
 _delta_time = 0
 
 
-def tick():
+def tick() -> None:
+    """Задает количество секунд, прошедших в прошлого кадра. Вызывается только в main!"""
     global _delta_time
     _delta_time = _clock.tick() / 1000
 
 
-def delta_time():
+def delta_time() -> float:
+    """Возвращает количество секунд, прошедших в прошлого кадра."""
     return _delta_time
