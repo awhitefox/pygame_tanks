c5e0adddcae0be3f6e091a11197b761bc732ac9e
AWhiteFox <ignat.k.150@gmail.com>
2021-01-17 12:10:50 +0300

    Document sounds.py

--- tanks/sounds.py
+++ tanks/sounds.py
@@ -4,7 +4,8 @@ VOLUME = 0.75
 pygame.mixer.init()
 
 
-def load_sound(filename):
+def load_sound(filename: str) -> pygame.mixer.Sound:
+    """Возвращает звук, загруженный из указанного файла. Поиск файла происходит в папке ./data/"""
     sound = pygame.mixer.Sound(os.path.join('data', filename))
     sound.set_volume(VOLUME)
     return sound
