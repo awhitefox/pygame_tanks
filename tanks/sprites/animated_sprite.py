cd8d1181c47fe40c0c86ccb2838d927fa9ff552c
AWhiteFox <ignat.k.150@gmail.com>
2021-01-17 21:43:55 +0300

    Add animated_sprite.py

---
+++ tanks/sprites/animated_sprite.py
@@ -0,0 +1,22 @@
+import pygame
+from tanks.time import delta_time
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    """Базовый класс анимированного спрайта, используется для наследования."""
+    frames = None
+    seconds_per_frame = None
+
+    def __init__(self, x: float, y: float, *groups: pygame.sprite.Group):
+        super().__init__(*groups)
+        self.frame_i = 0
+        self.seconds_from_frame_change = 0
+        self.image = self.frames[self.frame_i]
+        self.rect = pygame.Rect(x, y, *self.image.get_size())
+
+    def update(self) -> None:
+        self.seconds_from_frame_change += delta_time()
+        if self.seconds_from_frame_change >= self.seconds_per_frame:
+            self.frame_i = (self.frame_i + 1) % len(self.frames)
+            self.image = self.frames[self.frame_i]
+            self.seconds_from_frame_change = 0
